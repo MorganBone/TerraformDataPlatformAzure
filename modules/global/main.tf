6f2127d744e986ff27281253433537f91deeea32
Morgan Jeremy Bone <morgan.bone@wsa.com>
2025-03-21 17:17:59 +0800

    update doc files

---
+++ modules/global/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 1.10.3"
+  backend "azurerm" {}
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.14"
+    }
+  }
+}
+provider "azurerm" {
+  features {
+    key_vault {
+      recover_soft_deleted_key_vaults = true
+      purge_soft_delete_on_destroy    = true
+    }
+  }
+}
